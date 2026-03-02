246382fa973ac6b4d0b6c763273efe31bbaccd02
leandroomena18 <164086583+leandroomena18@users.noreply.github.com>
2026-03-02 14:59:04 -0300

    ADD variável ATUALIZAR_BASE_API em config.py para evitar atualizar o banco de dados
    Às vezes não é necessário atualizar o banco de dados, pois demora um pouco para atualizar os arquivos de vetores
    
    Mais para frente podemos programar para atualizar em um horário específico ou semanalmente

--- config.py
+++ config.py
@@ -2,7 +2,6 @@ import os
 from datetime import datetime
 
 # --- MAPEAMENTO INTELIGENTE DE PASTAS ---
-# Descobre automaticamente a pasta onde o projeto está rodando
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 PASTA_DADOS = os.path.join(BASE_DIR, "banco_de_dados_local")
 PASTA_CSV = os.path.join(BASE_DIR, "projetos_em_csv")
@@ -15,8 +14,12 @@ NOME = "Oasis"
 
 # 2. CONFIGURAÇÕES GERAIS DA IA
 CONSULTA_USUARIO = "Regulamentação inteligência artificial"
-DATA_INICIO_COLETA = datetime(2015, 1, 1) # Mudado para 2020 conforme você pediu
+DATA_INICIO_COLETA = datetime(2015, 1, 1) 
 MODELO_NOME = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
+
+# --- NOVO: INTERRUPTOR DA API ---
+# True = Conecta na Câmara e baixa projetos novos. False = Usa só o que já tem salvo (Muito mais rápido!)
+ATUALIZAR_BASE_API = False 
 
 # 3. PESOS E NOTAS DE CORTE DO FILTRO HÍBRIDO
 PESO_SEMANTICO = 0.8
